9c3e09161c26679d975493a39296f95abdd252f5
Zaid <iamzaidsheikh@gmail.com>
2023-03-08 16:25:43 +0530

    feat(nano-gpt): add implementation for multi headed self attention

--- bigram.py
+++ bigram.py
@@ -68,6 +68,16 @@ class Head(nn.Module):
         return out
 
 
+class MultiHeadAttention(nn.Module):
+    """multiple heads of attention in parallel"""
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
+
 # Bigram Langugage Model
 class BigramLanguageModel(nn.Module):
 
@@ -77,7 +87,7 @@ class BigramLanguageModel(nn.Module):
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         # encoding the position of the tokens
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
-        self.sa_head = Head(n_embed)
+        self.sa_heads = MultiHeadAttention(4, n_embed // 4) # 4 heads of 8-dimensional self-attention
         self.lm_head = nn.Linear(n_embed, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -86,7 +96,7 @@ class BigramLanguageModel(nn.Module):
         pos_embeds = self.position_embedding_table(torch.arange(T, device=device))  # (T, C)
         # x holds the token identities and also the positions at which these tokens occur
         x = token_embeds + pos_embeds  # (B, T, C)
-        x = self.sa_head(x)
+        x = self.sa_heads(x)
         logits = self.lm_head(x)  # (B, T, vocab_size)
 
         # loss function
