5f4f21abf56aedb46510ea92726284866874ea8a
Zaid <iamzaidsheikh@gmail.com>
2023-03-08 16:35:10 +0530

    feat(nano-gpt): add a feed forward network

--- bigram.py
+++ bigram.py
@@ -78,6 +78,19 @@ class MultiHeadAttention(nn.Module):
     def forward(self, x):
         return torch.cat([h(x) for h in self.heads], dim=-1)
 
+class FeedForward(nn.Module):
+    """simple linear layer followed by non-linearity"""
+
+    def __init__(self, n_embed):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embed, n_embed),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
 # Bigram Langugage Model
 class BigramLanguageModel(nn.Module):
 
@@ -88,6 +101,7 @@ class BigramLanguageModel(nn.Module):
         # encoding the position of the tokens
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
         self.sa_heads = MultiHeadAttention(4, n_embed // 4) # 4 heads of 8-dimensional self-attention
+        self.ffwd = FeedForward(n_embed)
         self.lm_head = nn.Linear(n_embed, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -97,6 +111,7 @@ class BigramLanguageModel(nn.Module):
         # x holds the token identities and also the positions at which these tokens occur
         x = token_embeds + pos_embeds  # (B, T, C)
         x = self.sa_heads(x)
+        x= self.ffwd(x)
         logits = self.lm_head(x)  # (B, T, vocab_size)
 
         # loss function
