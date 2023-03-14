caa2dab4ef034622d325e3c840228390c770b13c
Zaid <iamzaidsheikh@gmail.com>
2023-03-14 14:34:51 +0530

    feat(nano-gpt): update hyperparams to scale up the model

--- bigram.py
+++ bigram.py
@@ -3,17 +3,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyper parameters
-batch_size = 32
-block_size = 8
+batch_size = 64
+block_size = 256
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embed = 32
-n_head = 4
-n_layer = 4
-dropout = 0.0
+n_embed = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 
 # read the input dataset
 with open('data/tinyshakespeare.txt', 'r', encoding='utf-8') as f:
