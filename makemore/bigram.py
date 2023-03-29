e699511516d4ef3bd769c4ebc1d175e6c1782de6
Zaid <iamzaidsheikh@gmail.com>
2023-03-29 15:17:34 +0530

    feat(bigram): add regularization loss

--- makemore/bigram.py
+++ makemore/bigram.py
@@ -11,15 +11,23 @@ itoch = {i: ch for ch, i in chtoi.items()}
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-i', '--iters', help='Number of iterations', default=10)
-parser.add_argument('-l', '--lr', help='Learning Rate', default=1)
-parser.add_argument('-s', '--samples', help='Number of Samples', default=10)
+parser.add_argument(
+    '-i', '--iters', help='Number of iterations (int)', default=10)
+parser.add_argument('-l', '--lr', help='Learning Rate (float)', default=1)
+parser.add_argument('-s', '--samples',
+                    help='Number of Samples (int)', default=10)
+parser.add_argument('-r', '--reg',
+                    help='Regulaize Loss (bool)', default=True)
+parser.add_argument(
+    '-p', '--printw', help='Show weights (bool)', default=False)
 
 args = parser.parse_args()
 
 n_iters = int(args.iters)
-lr = int(args.lr)
+lr = float(args.lr)
 n_samples = int(args.samples)
+regularize_loss = bool(args.reg)
+show_weights = bool(args.printw)
 
 
 def get_datasets(words):
@@ -44,13 +52,21 @@ def train(n_iters, inputs, targets):
         # forward pass
         logits = inputs_enc @ w
         probs = torch.softmax(logits, dim=1)
-        loss = - probs[torch.arange(probs.size(dim=0)), targets].log().mean()
+        if regularize_loss:
+            loss = - probs[torch.arange(probs.size(dim=0)),
+                           targets].log().mean() + 0.01*(w**2).mean()
+        else:
+            loss = - probs[torch.arange(probs.size(dim=0)),
+                           targets].log().mean()
         print(loss.item())
 
         # backward pass
         w.grad = None
         loss.backward()
         w.data += -lr * w.grad
+
+        if show_weights:
+            print(f'first row of the weight matrix is:\n{w[:1]}\n')
 
     return w
 
